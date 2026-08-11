a61c07466dec07401aa667b45bad6e31f9b2d9bc
Bob Englert <mre29073@yahoo.com>
2026-08-11 14:07:38 -0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- js/Data_Media.js
+++ js/Data_Media.js
@@ -832,9 +832,9 @@ var VideoData = {
 	ordinary: {
 		ids: [
 			'CDljPsRIryo',
+			'Xa6eceuyYj8',
 			'EcxRvfTC2D8',
 			'FB0TWvag_Ic',
-			'Xa6eceuyYj8',
 			'K_WOFqap0oM',
 			'qlnC-SP4MeQ',
 			{'videoId':'_e1QRtxUF50', 'endSeconds': 290},
@@ -883,9 +883,9 @@ var VideoData = {
 
 		names: [
 			'We Are Called',
+			'Pange Lingua<br>ENCHANTING',
 			'Come To The Water<br>John Foley, Sunday 7pm Choir<br>Catholic & Christian Choral Music',
 			'Hosea - Come Back to me<br>John Michael Talbot',
-			'Pange Lingua<br>ENCHANTING',
 			'Come Holy Spirit<br>Tony Yu',
 			'Salve Regina<br>ENCHANTING',
 			'Behold The Lamb<br>Passion, Kristian Stanfill',
